b3f663388e714657ad0fff83b564bf7358adfedf
parrenoleendert <181639149+parrenoleendert@users.noreply.github.com>
2026-03-27 14:21:49 +0800

    add main.java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        Model model = new Model();
+        View view = new View();
+
+        new Controller(model, view);
+    }
+}
